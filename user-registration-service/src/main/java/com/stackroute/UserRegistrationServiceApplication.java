fbe2ea760a88e173ac82aa7044f2e15766b4ca82
Monishaa <monishaa2701@gmail.com>
2019-06-19 17:58:31 +0530

    docker-compose

--- user-registration-service/src/main/java/com/stackroute/UserRegistrationServiceApplication.java
+++ user-registration-service/src/main/java/com/stackroute/UserRegistrationServiceApplication.java
@@ -1,10 +1,7 @@
 package com.stackroute;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 /* @SpringBootApplication -- > Either this or the below mentioned three annotations... */
 
@@ -14,11 +11,11 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 
-public class UserserviceApplication {
+public class UserRegistrationServiceApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(UserserviceApplication.class, args);
+		SpringApplication.run(UserRegistrationServiceApplication.class, args);
 	}
 
 }
