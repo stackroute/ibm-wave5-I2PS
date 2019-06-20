68d196dfcf145344641617f2ac85df6b08fb4a7c
sushmitha <sushmitha@stackroute.in>
2019-06-20 12:38:08 +0530

    changes in user-login-service

--- user-login-service/src/test/java/com/stackroute/userloginservice/UserLoginServiceApplicationTests.java
+++ user-login-service/src/test/java/com/stackroute/userloginservice/UserLoginServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.stackroute;
+package com.stackroute.userloginservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UserserviceApplicationTests {
+public class UserLoginServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
