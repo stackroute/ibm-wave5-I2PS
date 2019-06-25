f8680946e334d822b3aa8dc9591add8588ab777a
chaithra <chaitragshetty>
2019-06-25 10:37:12 +0530

    innovator-profile frontend added

---
+++ angular-frontend/src/app/innovator-profile-registration/innovator-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-innovator-profile',
+  templateUrl: './innovator-profile.component.html',
+  styleUrls: ['./innovator-profile.component.css']
+})
+export class InnovatorProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
