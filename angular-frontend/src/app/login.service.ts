9dd9a5d8afdd2715d3f3ee41a66f0d5553d340d4
prerna0001 <prernachauhan0001@gmail.com>
2019-06-25 11:52:15 +0530

    login service modified

--- angular-frontend/src/app/login.service.ts
+++ angular-frontend/src/app/login.service.ts
@@ -12,7 +12,12 @@ export class LoginService {
   authenticateUser(user):any
   {
     console.log(user);
-    this.http.post("http://localhost:8102/api/v1/user",user).subscribe();
+    this.http.post("http://localhost:8088/api/v1/user",user).subscribe((user:any) => 
+      {
+      console.log(user);
+         }
+                                                                                                                                                                                                                                                                                                                       );
     console.log(user);
+
   }
 }
