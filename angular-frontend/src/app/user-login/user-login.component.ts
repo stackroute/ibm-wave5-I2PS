7462527621c4a0193583091250dd3bada536b570
prerna0001 <prernachauhan0001@gmail.com>
2019-06-26 11:49:07 +0530

    login front end page chnaged

--- angular-frontend/src/app/user-login/user-login.component.ts
+++ angular-frontend/src/app/user-login/user-login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import { LoginService } from '../login.service';
 
-import  { JwtHelperService } from '@auth0/angular-jwt';
+// import  { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-user-login',
@@ -16,7 +16,7 @@ export class UserLoginComponent implements OnInit {
   constructor(private loginService:LoginService, private route: ActivatedRoute, private router:Router) { }
   arrayOfUser:any=[];
 
-  helper = new JwtHelperService();
+  // helper = new JwtHelperService();
 
   ngOnInit() {
     console.log("in ngoninit")
@@ -31,26 +31,26 @@ export class UserLoginComponent implements OnInit {
     this.loginService.authenticateUser(userdata);
   
    
-    this.loginService.authenticateUser(userdata).subscribe(data => {
-    console.log(data)
-    if (data.token) {
+  //   this.loginService.authenticateUser(userdata).subscribe(data => {
+  //   console.log(data)
+  //   if (data.token) {
 
-     let role =  this.helper.decodeToken(data.token).aud;
-     console.log(data.token);
-     if (role === 'Innovator') {
-      this.router.navigateByUrl('/innovator-profile')
-     }
-     if(role === '!Innovator') {
-      this.router.navigateByUrl('/service-provider')
-     }
-    }
-    else{
-      this.loggedIn = true;
-    }
+  //    let role =  this.helper.decodeToken(data.token).aud;
+  //    console.log(data.token);
+  //    if (role === 'Innovator') {
+  //     this.router.navigateByUrl('/innovator-profile')
+  //    }
+  //    if(role === '!Innovator') {
+  //     this.router.navigateByUrl('/service-provider')
+  //    }
+  //   }
+  //   else{
+  //     this.loggedIn = true;
+  //   }
 
-  }, err  => {
-    this.loggedIn = true;
-  })
+  //   }, err  => {
+  //     this.loggedIn = true;
+  //   })
 }
 
 }
