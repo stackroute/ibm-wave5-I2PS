a9f63f6d6d932829d1637e75a9e245e9dee528df
chaithra <chaitragshetty>
2019-07-02 09:27:41 +0530

    service-provider dashboard added

---
+++ angular-frontend/src/app/service-profile/service-profile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-profile',
+  templateUrl: './service-profile.component.html',
+  styleUrls: ['./service-profile.component.css']
+})
+export class ServiceProfileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
