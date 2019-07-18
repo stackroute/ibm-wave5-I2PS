e10abd5b313e03b2bcb540c7fae08b83cedd90e3
Monishaa <monishaa2701@gmail.com>
2019-07-18 12:57:21 +0530

    changes in user-login-service pom.xml

--- innovator-management-service/src/main/java/com/stackroute/innovatormanagementservice/InnovatorManagementServiceApplication.java
+++ innovator-management-service/src/main/java/com/stackroute/innovatormanagementservice/InnovatorManagementServiceApplication.java
@@ -3,10 +3,12 @@ package com.stackroute.innovatormanagementservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+
 public class InnovatorManagementServiceApplication {
 
 	public static void main(String[] args) {
