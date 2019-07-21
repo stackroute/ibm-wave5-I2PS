8075e095914591bab9d9af1ea36a39f9fa0dd01f
anngargi@gmail.com <anngargi@gmail.com>
2019-07-21 19:24:35 +0530

    innovator ans service provider registrations fixed

---
+++ service-provider-profile-service/src/main/java/com/stackroute/serviceproviderprofileservice/domain/dumps.java
@@ -0,0 +1,9 @@
+package com.stackroute.serviceproviderprofileservice.domain;
+
+
+class dumps {
+    public static void main(String[] args) {
+        Thread t = new Thread();
+        System.out.println(t.currentThread().getPriority());
+    }
+}
