7ae996eebd0ddd3e7bc5c7b711aac4472d30890c
anngargi@gmail.com <anngargi@gmail.com>
2019-07-22 09:46:07 +0530

    modified idea detail

--- angular-frontend/src/app/user-login/user-login.component.ts
+++ angular-frontend/src/app/user-login/user-login.component.ts
@@ -72,4 +72,3 @@ export class UserLoginComponent implements OnInit {
   }
 
 }
-
--- service-provider-profile-service/src/main/java/com/stackroute/serviceproviderprofileservice/domain/dumps.java
+++
@@ -1,9 +0,0 @@
-package com.stackroute.serviceproviderprofileservice.domain;
-
-
-class dumps {
-    public static void main(String[] args) {
-        Thread t = new Thread();
-        System.out.println(t.currentThread().getPriority());
-    }
-}
