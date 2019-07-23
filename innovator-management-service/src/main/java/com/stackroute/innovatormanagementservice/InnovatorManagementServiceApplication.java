88db6da5db76d9a649387a436b9fbc4374356860
Monishaa <monishaa2701@gmail.com>
2019-07-23 10:29:54 +0530

    angular-modified

--- innovator-management-service/src/main/java/com/stackroute/innovatormanagementservice/InnovatorManagementServiceApplication.java
+++ innovator-management-service/src/main/java/com/stackroute/innovatormanagementservice/InnovatorManagementServiceApplication.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
 public class InnovatorManagementServiceApplication {
 
 	public static void main(String[] args) {
