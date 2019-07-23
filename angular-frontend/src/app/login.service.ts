b458808e19a8e796ba2c4a5e614173b3328f8566
Monishaa <monishaa2701@gmail.com>
2019-07-23 12:23:14 +0530

    changes in url in angular

--- angular-frontend/src/app/login.service.ts
+++ angular-frontend/src/app/login.service.ts
@@ -22,6 +22,6 @@ export class LoginService {
   }
 
   getRecommendations(emailId):any{
-    return this.http.get(`http://l13.232.118.81:8080/recommendationservice/rest/neo4j/idea/ideas/${emailId}`);
+    return this.http.get(`http://13.232.118.81:8080/recommendationservice/rest/neo4j/idea/ideas/${emailId}`);
   }
 }
