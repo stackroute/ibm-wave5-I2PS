65189b09ae49c767241aa63f9b3e073148be0c42
Monishaa <monishaa2701@gmail.com>
2019-07-25 08:32:34 +0530

    last modification

--- idea-hub-service/src/main/java/com/stackroute/SeedData/SeedData.java
+++ idea-hub-service/src/main/java/com/stackroute/SeedData/SeedData.java
@@ -1,79 +1,79 @@
-//package com.stackroute.SeedData;
-//import com.stackroute.domain.Idea;
-//import com.stackroute.repository.IdeaHubRepository;
-//import com.stackroute.service.IdeaHubService;
-//import org.apache.poi.ss.usermodel.Cell;
-//import org.apache.poi.ss.usermodel.Row;
-//import org.apache.poi.xssf.usermodel.XSSFSheet;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.ApplicationListener;
-//import org.springframework.context.event.ContextRefreshedEvent;
-//import org.springframework.stereotype.Component;
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.util.Iterator;
-//import java.util.List;
-//
-//@Component
-//public class SeedData implements ApplicationListener<ContextRefreshedEvent> {
-//
-//    @Autowired
-//    IdeaHubRepository ideaHubRepository;
-//    @Autowired
-//    IdeaHubService ideaHubService;
-//    @Override
-//    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-//
-//        try {
-//
-//            FileInputStream file = new FileInputStream(new File("Seed_Ideas1.xlsx"));
-//
-//            XSSFWorkbook workbook = new XSSFWorkbook(file);
-//
-//            XSSFSheet sheet = workbook.getSheetAt(0);
-//
-//            Iterator<Row> rowIterator = sheet.iterator();
-//            // Traversing over each row of XLSX file
-////            rowIterator.next();//Skipping 1st line
-//           while (rowIterator.hasNext()) {
-//
-//               Row row = rowIterator.next();
-//
-//               // For each row, iterate through each columns
-//               Iterator<Cell> cellIterator = row.cellIterator();
-//               for (int i = 1; cellIterator.hasNext(); i++) {
-//                   Idea ideas = new Idea();
-//                   for (int j = 0; j <= sheet.getLeftCol(); j++) {
-//                       int count =100;
-//
-//
-//                       ideas.setTitle(workbook.getSheetAt(0).getRow(i).getCell(j + 0).toString());
-//                       ideas.setDomain(workbook.getSheetAt(0).getRow(i).getCell(j + 1).toString());
-//                       ideas.setSubDomain(workbook.getSheetAt(0).getRow(i).getCell(j + 2).toString());
-//                       ideas.setDescription(workbook.getSheetAt(0).getRow(i).getCell(j + 3).toString());
-//                       //idea1.setBudget(Double.parseDouble(workbook.getSheetAt(0).getRow(i).getCell(j+4)+"\n"));
-//                       ideas.setEmailId(workbook.getSheetAt(0).getRow(i).getCell(j + 7).toString());
-//                       ideas.setRoles(List.of(workbook.getSheetAt(0).getRow(i).getCell(j + 5).toString().split(",")));
-//                       ideaHubRepository.save(ideas);
-//                       System.out.println(ideas);
-//                       ideaHubService.send(ideas);
-//
-//                   }
-//
-//               }
-//           }
-//
-//                 file.close();
-//
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//
-//}
-//
-//
-//
+package com.stackroute.SeedData;
+import com.stackroute.domain.Idea;
+import com.stackroute.repository.IdeaHubRepository;
+import com.stackroute.service.IdeaHubService;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Iterator;
+import java.util.List;
+
+@Component
+public class SeedData implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Autowired
+    IdeaHubRepository ideaHubRepository;
+    @Autowired
+    IdeaHubService ideaHubService;
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+        try {
+
+            FileInputStream file = new FileInputStream(new File("Seed_Ideas1.xlsx"));
+
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            Iterator<Row> rowIterator = sheet.iterator();
+            // Traversing over each row of XLSX file
+//            rowIterator.next();//Skipping 1st line
+           while (rowIterator.hasNext()) {
+
+               Row row = rowIterator.next();
+
+               // For each row, iterate through each columns
+               Iterator<Cell> cellIterator = row.cellIterator();
+               for (int i = 1; cellIterator.hasNext(); i++) {
+                   Idea ideas = new Idea();
+                   for (int j = 0; j <= sheet.getLeftCol(); j++) {
+                       int count =100;
+
+
+                       ideas.setTitle(workbook.getSheetAt(0).getRow(i).getCell(j + 0).toString());
+                       ideas.setDomain(workbook.getSheetAt(0).getRow(i).getCell(j + 1).toString());
+                       ideas.setSubDomain(workbook.getSheetAt(0).getRow(i).getCell(j + 2).toString());
+                       ideas.setDescription(workbook.getSheetAt(0).getRow(i).getCell(j + 3).toString());
+                       //idea1.setBudget(Double.parseDouble(workbook.getSheetAt(0).getRow(i).getCell(j+4)+"\n"));
+                       ideas.setEmailId(workbook.getSheetAt(0).getRow(i).getCell(j + 7).toString());
+                       ideas.setRoles(List.of(workbook.getSheetAt(0).getRow(i).getCell(j + 5).toString().split(",")));
+                       ideaHubRepository.save(ideas);
+                       System.out.println(ideas);
+                       ideaHubService.send(ideas);
+
+                   }
+
+               }
+           }
+
+                 file.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+
+
--- service-provider-profile-service/src/main/java/com/stackroute/serviceproviderprofileservice/SeedData/SeedData.java
+++ service-provider-profile-service/src/main/java/com/stackroute/serviceproviderprofileservice/SeedData/SeedData.java
@@ -1,78 +1,78 @@
-//package com.stackroute.serviceproviderprofileservice.SeedData;
-//
-//import com.stackroute.serviceproviderprofileservice.domain.ServiceProvider;
-//import com.stackroute.serviceproviderprofileservice.repository.ServiceProviderRepository;
-//import com.stackroute.serviceproviderprofileservice.service.ServiceProviderService;
-//import org.apache.poi.ss.usermodel.Cell;
-//import org.apache.poi.ss.usermodel.Row;
-//import org.apache.poi.xssf.usermodel.XSSFSheet;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.ApplicationListener;
-//import org.springframework.context.event.ContextRefreshedEvent;
-//import org.springframework.stereotype.Component;
-//
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.math.BigDecimal;
-//import java.util.Iterator;
-//import java.util.List;
-//@Component
-//public class SeedData implements ApplicationListener<ContextRefreshedEvent>{
-//        @Autowired
-//    ServiceProviderRepository serviceProviderRepository;
-//        @Autowired
-//    ServiceProviderService serviceProviderService;
-//        @Override
-//        public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-//
-//            try {
-//
-//                FileInputStream file = new FileInputStream(new File("Seed_Ideas2.xlsx"));
-//
-//                XSSFWorkbook workbook = new XSSFWorkbook(file);
-//
-//                XSSFSheet sheet = workbook.getSheetAt(0);
-//
-//                Iterator<Row> rowIterator = sheet.iterator();
-//
-////            rowIterator.next();//Skipping 1st line
-//                while (rowIterator.hasNext()) {
-//
-//                    Row row = rowIterator.next();
-//                    Iterator<Cell> cellIterator = row.cellIterator();
-//                    for (int i = 1; cellIterator.hasNext(); i++) {
-//                         ServiceProvider service=new ServiceProvider();
-//                        for (int j = 0; j <= sheet.getLeftCol(); j++) {
-//                            int count =100;
-//
-//                            service.setEmailId(workbook.getSheetAt(0).getRow(i).getCell(j + 0).toString());
-//                            service.setPassword(workbook.getSheetAt(0).getRow(i).getCell(j + 1).toString());
-//                            service.setName(workbook.getSheetAt(0).getRow(i).getCell(j + 2).toString());
-//                            service.setDomain(workbook.getSheetAt(0).getRow(i).getCell(j + 3).toString());
-//                            service.setSubDomain(List.of(workbook.getSheetAt(0).getRow(i).getCell(j + 4).toString()));
-//                            service.setRole(workbook.getSheetAt(0).getRow(i).getCell(j + 5).toString());
-//                            service.setSkills(List.of(workbook.getSheetAt(0).getRow(i).getCell(j + 6).toString().split(",")));
-//                            service.setAbout(workbook.getSheetAt(0).getRow(i).getCell(j + 7).toString());
-////                            service.setChargePerHour(workbook.getSheetAt(0).getRow(i).getCell(j + 9).toString());
-//                            service.setChargePerHour(new BigDecimal(count+100));
-//                            serviceProviderRepository.save(service);
-//                            System.out.println(service);
-//                            serviceProviderService.send(service);
-//
-//                        }
-//
-//                    }
-//                }
-//
-//                file.close();
-//
-//
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//
-//    }
-//
+package com.stackroute.serviceproviderprofileservice.SeedData;
+
+import com.stackroute.serviceproviderprofileservice.domain.ServiceProvider;
+import com.stackroute.serviceproviderprofileservice.repository.ServiceProviderRepository;
+import com.stackroute.serviceproviderprofileservice.service.ServiceProviderService;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigDecimal;
+import java.util.Iterator;
+import java.util.List;
+@Component
+public class SeedData implements ApplicationListener<ContextRefreshedEvent>{
+        @Autowired
+    ServiceProviderRepository serviceProviderRepository;
+        @Autowired
+    ServiceProviderService serviceProviderService;
+        @Override
+        public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+            try {
+
+                FileInputStream file = new FileInputStream(new File("Seed_Ideas2.xlsx"));
+
+                XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+                XSSFSheet sheet = workbook.getSheetAt(0);
+
+                Iterator<Row> rowIterator = sheet.iterator();
+
+//            rowIterator.next();//Skipping 1st line
+                while (rowIterator.hasNext()) {
+
+                    Row row = rowIterator.next();
+                    Iterator<Cell> cellIterator = row.cellIterator();
+                    for (int i = 1; cellIterator.hasNext(); i++) {
+                         ServiceProvider service=new ServiceProvider();
+                        for (int j = 0; j <= sheet.getLeftCol(); j++) {
+                            int count =100;
+
+                            service.setEmailId(workbook.getSheetAt(0).getRow(i).getCell(j + 0).toString());
+                            service.setPassword(workbook.getSheetAt(0).getRow(i).getCell(j + 1).toString());
+                            service.setName(workbook.getSheetAt(0).getRow(i).getCell(j + 2).toString());
+                            service.setDomain(workbook.getSheetAt(0).getRow(i).getCell(j + 3).toString());
+                            service.setSubDomain(List.of(workbook.getSheetAt(0).getRow(i).getCell(j + 4).toString()));
+                            service.setRole(workbook.getSheetAt(0).getRow(i).getCell(j + 5).toString());
+                            service.setSkills(List.of(workbook.getSheetAt(0).getRow(i).getCell(j + 6).toString().split(",")));
+                            service.setAbout(workbook.getSheetAt(0).getRow(i).getCell(j + 7).toString());
+//                            service.setChargePerHour(workbook.getSheetAt(0).getRow(i).getCell(j + 9).toString());
+                            service.setChargePerHour(new BigDecimal(count+100));
+                            serviceProviderRepository.save(service);
+                            System.out.println(service);
+                            serviceProviderService.send(service);
+
+                        }
+
+                    }
+                }
+
+                file.close();
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+    }
+
