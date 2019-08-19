16c5f1308a859d7b4a4c99d065249ba46fa5c399
MeghnaW19 <meghnanvt70@gmail.com>
2019-08-19 12:37:09 +0530

    Update recent-ideas.service.ts

--- angular-frontend/src/app/recent-ideas.service.ts
+++ angular-frontend/src/app/recent-ideas.service.ts
@@ -10,17 +10,17 @@ export class RecentIdeasService {
   constructor(private httpclient: HttpClient) { }
 
   getRecentIdeas():any{
-    return this.httpclient.get("http://13.232.118.81:8080/ideahubservice/api/v1/ideas");
+    return this.httpclient.get("http://140.238.163.105:8080/ideahubservice/api/v1/ideas");
   }
 
   getIdeaDetails(value):any{
     console.log(value);
-    return this.httpclient.get(`http://13.232.118.81:8080/ideahubservice/api/v1/idea/${value}`)
+    return this.httpclient.get(`http://140.238.163.105:8080/ideahubservice/api/v1/idea/${value}`)
   }
 
   sendToInnovator(title,emailId):any{
-    this.httpclient.put(`http://13.232.118.81:8080/innovatormanagementservice/api/v1/idea/${title}/${emailId}`,{}).subscribe();
-    this.httpclient.post(`http://13.232.118.81:8080/recommendationservice/rest/neo4j/idea/user/${title}/${emailId}`,{}).subscribe();
+    this.httpclient.put(`http://140.238.163.105:8080/innovatormanagementservice/api/v1/idea/${title}/${emailId}`,{}).subscribe();
+    this.httpclient.post(`http://140.238.163.105:8080/recommendationservice/rest/neo4j/idea/user/${title}/${emailId}`,{}).subscribe();
     console.log("came");
 
   }
@@ -28,12 +28,12 @@ export class RecentIdeasService {
   getAppliedServiceProviders(title):any
   {
     console.log("here applied people",title)
-    return this.httpclient.get(`http://13.232.118.81:8080/innovatormanagementservice/api/v1//serviceProviders/${title}`);
+    return this.httpclient.get(`http://140.238.163.105:8080/innovatormanagementservice/api/v1//serviceProviders/${title}`);
     console.log("after url")
   }
 
   approveOrRejectSP(title,emailId,status):any{
-this.httpclient.put(`http://13.232.118.81:8080/innovatormanagementservice/api/v1/serviceProviders/${title}/${emailId}/${status}`,{}).subscribe();
+this.httpclient.put(`http://140.238.163.105:8080/innovatormanagementservice/api/v1/serviceProviders/${title}/${emailId}/${status}`,{}).subscribe();
 console.log("coming");
   }
 }
