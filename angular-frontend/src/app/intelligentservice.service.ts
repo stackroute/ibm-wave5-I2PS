0ff7c4ef908261a62f2c8112bd96194305e0f7f9
MeghnaW19 <meghnanvt70@gmail.com>
2019-08-19 12:38:50 +0530

    Update intelligentservice.service.ts

--- angular-frontend/src/app/intelligentservice.service.ts
+++ angular-frontend/src/app/intelligentservice.service.ts
@@ -14,7 +14,7 @@ export class IntelligentserviceService {
 
   getServiceProviders(value):any {
     console.log("in intelligent service")
-    var url=`http://13.232.118.81:8080/intelligentservice/api/v1/intelligentService/${value}`;
+    var url=`http://140.238.163.105:8080/intelligentservice/api/v1/intelligentService/${value}`;
     console.log("after the url")
     return this.http.get(url);
   }
